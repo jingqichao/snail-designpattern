8fffe703213897be8a79dcf9c6b5b9b491ced202
荆启超 <1209446716@qq.com>
2020-09-14 21:05:57 +0800

    单例模式：懒汉式（线程安全，同步方法）

---
+++ design01_Singleton/src/main/java/com/jqc/singleton04/SingletonTest04.java
@@ -0,0 +1,32 @@
+package com.jqc.singleton04;
+
+
+public class SingletonTest04 {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getSingleton();
+        Singleton singleton2 = Singleton.getSingleton();
+        System.out.println(singleton1 == singleton2);//true
+        System.out.println(singleton1);//com.jqc.singleton.Singleton@1b6d3586
+        System.out.println(singleton2);//com.jqc.singleton.Singleton@1b6d3586
+        System.out.println(singleton1.hashCode());//460141958
+        System.out.println(singleton2.hashCode());//460141958
+    }
+}
+
+//懒汉式（线程不安全）
+ class Singleton {
+    //1.构造器私有化
+    private Singleton() {
+    }
+
+    //2.本类内部创建对象实例
+    private static Singleton singleton;
+
+    //3.提供一个静态的公有方法，当使用到该方法的时候，才去创建instance（加入同步代码方法）
+    public static synchronized Singleton getSingleton() {
+        if (null == singleton) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
